1dee147fe13207448ddca8295f979172bd6cceb4
brisapanichelli <135248003+brisapanichelli@users.noreply.github.com>
2024-05-15 19:05:46 -0300

    actualización de script de google traductor

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ document.addEventListener('DOMContentLoaded', function () {
       title.setAttribute('aria-expanded', !expanded);
     });
   });
+document.getElementById('toggle-language').addEventListener('click', function () {
+  const googleTranslateElement = document.getElementById('google_translate_element');
+  const currentLanguage = googleTranslateElement.getAttribute('data-language');
 
-  document.getElementById('toggle-language').addEventListener('click', function () {
-    const googleTranslateElement = document.getElementById('google_translate_element');
-    const currentLanguage = googleTranslateElement.getAttribute('data-language');
-
-    if (currentLanguage === 'es') {
-      googleTranslateElement.setAttribute('data-language', 'en');
-      googleTranslateElement.innerHTML = '';
-      new google.translate.TranslateElement({ pageLanguage: 'es', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
-      this.textContent = 'Change to English';
-    } else {
-      googleTranslateElement.setAttribute('data-language', 'es');
-      googleTranslateElement.innerHTML = '';
-      new google.translate.TranslateElement({ pageLanguage: 'es', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
-      this.textContent = 'Cambiar a Inglés';
-    }
-  });
+  if (currentLanguage === 'es') {
+    googleTranslateElement.setAttribute('data-language', 'en');
+    googleTranslateElement.innerHTML = ''; // Clear existing content
+    // No need to recreate the element, update its configuration instead
+    new google.translate.TranslateElement({ pageLanguage: 'en', includedLanguages: 'es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }); 
+    this.textContent = 'Change to English';
+  } else {
+    googleTranslateElement.setAttribute('data-language', 'es');
+    googleTranslateElement.innerHTML = '';  // Clear existing content
+          // No need to recreate the element, update its configuration instead
+    new google.translate.TranslateElement({ pageLanguage: 'es', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE });
+    this.textContent = 'Cambiar a Inglés';
+  }
 });
